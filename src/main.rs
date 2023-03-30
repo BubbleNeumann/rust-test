a8b37aa75b9453270b70c02f5a68c798097c8bab
BubbleNeumann <bubbleneumann@gmail.com>
2023-03-30 10:03:50 +0400

    fix: pass func as parameter

--- src/main.rs
+++ src/main.rs
@@ -20,23 +20,26 @@ use std::cmp::min;
 use std::io::stdin;
 use std::thread::{self, JoinHandle};
 
+fn f(t: &i32) -> i32 {
+    t.pow(2)
+}
 
-fn spawn_upon_threshold(vec: &Vec<i32>) -> Vec<i32> {
+fn spawn_upon_threshold(vec: &Vec<i32>, f: fn(&i32) -> i32) -> Vec<i32> {
     // consider threshold to be a computational limitation of 1 thread
     const THRESHOLD: usize = 5;
 
-    let f = |t: &i32| t.pow(2);
     let f_caller = move |vec: &Vec<i32>| vec.iter().map(|x| f(x)).collect::<Vec<i32>>();
 
-    let mut res: Vec<i32> = f_caller(&vec[0..min(THRESHOLD, vec.len())].to_vec());
-
-    // spawn threads and put them into vector
+    // if size is above threshold, spawn threads first, so we don't waste time
     let mut handles: Vec<JoinHandle<Vec<i32>>> = vec![];
     for i in 1..vec.len() / THRESHOLD + (vec.len() % THRESHOLD > 0) as usize {
         let chunk = vec[i * THRESHOLD..min((i + 1) * THRESHOLD, vec.len())].to_vec();
         let handle = thread::spawn(move || f_caller(&chunk));
         handles.push(handle);
     }
+
+    // handle the first chunk of the input vector
+    let mut res: Vec<i32> = f_caller(&vec[0..min(THRESHOLD, vec.len())].to_vec());
 
     // merge threads if any were spawn and concat their return values into res
     for handle in handles {
@@ -55,45 +58,45 @@ fn main() {
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
 
-    println!("{:?}", spawn_upon_threshold(&vec));
+    println!("{:?}", spawn_upon_threshold(&vec, f));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::spawn_upon_threshold;
+    use crate::{ spawn_upon_threshold, f };
 
     #[test]
     fn test_spawn_upon_threshold_empty_vec() {
         let inp = vec![];
         let expected = vec![];
-        assert_eq!(spawn_upon_threshold(&inp), expected);
+        assert_eq!(spawn_upon_threshold(&inp, f), expected);
     }
 
     #[test]
     fn test_spawn_upon_threshold_below_threshold() {
         let inp = vec![1, 2, 3];
         let expected = vec![1, 4, 9];
-        assert_eq!(spawn_upon_threshold(&inp), expected);
+        assert_eq!(spawn_upon_threshold(&inp, f), expected);
     }
 
     #[test]
     fn test_spawn_upon_threshold_eq_to_threshold() {
         let inp = vec![1, 2, 3, 4, 5];
         let expected = vec![1, 4, 9, 16, 25];
-        assert_eq!(spawn_upon_threshold(&inp), expected);
+        assert_eq!(spawn_upon_threshold(&inp, f), expected);
     }
 
     #[test]
     fn test_spawn_upon_threshold_eq_to_double_threshold() {
         let inp = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let expected = vec![1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-        assert_eq!(spawn_upon_threshold(&inp), expected);
+        assert_eq!(spawn_upon_threshold(&inp, f), expected);
     }
 
     #[test]
     fn test_spawn_upon_threshold_above_double_threshold() {
-        let inp = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        let expected = vec![1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
-        assert_eq!(spawn_upon_threshold(&inp), expected);
+        let inp = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+        let expected = vec![1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121];
+        assert_eq!(spawn_upon_threshold(&inp, f), expected);
     }
 }
